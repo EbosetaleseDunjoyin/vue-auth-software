d519f4158f2ecd8c40e5bdd543fc09ed0d40147e
edje12021 <eboselese@gmail.com>
2023-03-03 11:03:46 +0100

    Api comments

--- routes/api.php
+++ routes/api.php
@@ -19,10 +19,13 @@ use App\Http\Controllers\Api\VerificationController;
 // Authenticate User
 Route::post('/auth/register', [AuthController::class, 'createUser']);
 Route::post('/auth/login', [AuthController::class, 'loginUser']);
+
+//Forgot Password
 Route::post('/auth/forgot-password', [AuthController::class, 'forgotPassword']);
 Route::post('/auth/verify-password-token', [AuthController::class, 'verifyPasswordResetToken']);
 Route::put('/auth/reset-password', [AuthController::class, 'resetPassword']);
 
+//Send SMS
 Route::post('/send-sms-otp', [AuthController::class, 'sendOtp']);
 Route::post('/send-email-otp', [AuthController::class, 'sendverification']);
 Route::post('/verify-otp', [AuthController::class, 'verifyOtp']);
@@ -37,15 +40,22 @@ Route::get('/loginissues', function (){
     ], 400);
 })->name('login');
 
+//Reset Password
 Route::post('/auth/reset-password', [AuthController::class, 'resetPassword'])->middleware('auth:sanctum');
+
+//Api sanctom Authenticated routes
 Route::group(['middleware' => ['auth:sanctum','is_verified']], function() {
-    
+    // Get Profile with token
     Route::get('/user/profile', [UserController::class , 'userProfile']);
+    //Update Username
     Route::put('/user/update-username', [UserController::class, 'updateUsername']);
+    //Create intrests
     Route::post('/user/create-intrests', [UserController::class, 'createIntrests']);
-    
+    //Update Password
     Route::put('/user/update-password', [AuthController::class, 'updatePassword']);
+    //Update Email
     Route::put('/user/update-email', [AuthController::class, 'updateEmail']);
+    //Logout User
     Route::get('/auth/logout', [AuthController::class, 'logoutUser']);
     
 });
